69339f98100d1a4b7471594229b3d5eb3b8e2056
AadelAferyad <animevineu@gmail.com>
2024-02-08 03:06:17 +0100

    user class

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this file is for user class """
+from .base_model import BaseModel
+
+class User(BaseModel):
+    """
+    this class is for user data
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
