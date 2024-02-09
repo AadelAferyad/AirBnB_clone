2955a6f01fbc9132e20a28fd3311b4c9907c75ee
AadelAferyad <animevineu@gmail.com>
2024-02-09 15:11:41 +0100

    pycodestyle fixed

--- models/base_model.py
+++ models/base_model.py
@@ -22,28 +22,32 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             storage.new(self)
 
     def __str__(self):
         """
         for printing information about the class
         """
-        return ("[" + self.__class__.__name__ + "] " + "(" + self.id + ") " + str(self.__dict__))
+        class_name = self.__class__.__name__
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
     def save(self):
         """
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute updated_at
+        with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
         """
         dictionary = dict(self.__dict__)
-        dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         return (dictionary)
