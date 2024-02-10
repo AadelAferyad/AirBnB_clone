3e2b1d5c24089c39b52592d2599d3b47625eaa5d
AadelAferyad <animevineu@gmail.com>
2024-02-10 17:25:56 +0100

    storage import chaged

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 this file containe the base class
 """
 
-from . import storage
+import models
 from datetime import datetime
 import uuid
 
@@ -39,7 +39,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -54,8 +54,8 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
